6fa9c225ab31b0ffc5e7ad91c2b5ceb786638a4e
你慧快乐 <tianxiang230@163.com>
2018-01-28 13:41:34 +0800

    添加配置

---
+++ src/main/java/com/tx/coin/config/BinancePropertyConfig.java
@@ -0,0 +1,12 @@
+package com.tx.coin.config;
+
+/**
+ * @author 你慧快乐
+ * @version V1.0
+ * @Package com.tx.coin.config
+ * @Description
+ * @date 2018-1-27 15:24
+ */
+public class BinancePropertyConfig {
+
+}
